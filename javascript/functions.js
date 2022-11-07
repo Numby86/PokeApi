401e752eb850a6367399041dbc86365feb868698
Numby86 <numby86@gmail.com>
2022-11-07 14:23:00 +0100

    feat(pokeapi)flipCards

--- javascript/functions.js
+++ javascript/functions.js
@@ -3,7 +3,7 @@
 const pokemonContainer = document.querySelector('#list-pokemons');
 const pokeList$$ = document.querySelector('#pokeList');
 
-pokeList$$.addEventListener('click', allPoke);
+// pokeList$$.addEventListener('click', allPoke);
 
 function allPoke() {
     searchPokemons();
@@ -56,5 +56,49 @@ const createPokemon = (pokemon, id) => {
     else{
         spriteContainer.classList.add(type1);
     }
-    pokemonContainer.appendChild(card);
+
+    const flipCard = document.createElement("div");
+    flipCard.classList.add("flip-card");
+
+    const cardContainer = document.createElement("div");
+    cardContainer.classList.add("card-container");
+
+    flipCard.appendChild(cardContainer);
+
+    const cardBack = document.createElement("div");
+    cardBack.classList.add("card-back");
+
+    const pokeTypes = document.createElement('h3')
+    const typesPoke = pokemon.types.map((tipos, index) => {
+        return tipos.type.name;
+    });
+    if(type2 === 'flying' && type1 === 'normal') {
+        pokeTypes.classList.add(type2);
+    }
+    else if(type2 === 'steel'){
+        pokeTypes.classList.add(type2);
+    }
+    else{
+        pokeTypes.classList.add(type1);
+    }
+    pokeTypes.textContent = typesPoke;
+
+    const divFeatures = document.createElement('div');
+    divFeatures.classList.add('features');
+
+    const abilityPoke = document.createElement('p');
+    abilityPoke.textContent = '<<abi: ' + pokemon.abilities[0].ability.name + '>>';
+    divFeatures.appendChild(abilityPoke);
+
+    const movePoke = document.createElement('p');
+    movePoke.textContent = '<<move: ' + pokemon.moves[0].move.name + '>>';
+    divFeatures.appendChild(movePoke);
+
+    cardBack.appendChild(pokeTypes);
+    cardBack.appendChild(divFeatures);
+
+    cardContainer.appendChild(card);
+    cardContainer.appendChild(cardBack);
+    pokemonContainer.appendChild(flipCard);
 }
+searchPokemons();
