0c4d060ec78b67fc35e854d40ccfdc95e5c98a0a
Numby86 <numby86@gmail.com>
2022-11-08 13:17:45 +0100

    feat(pokeapi)game-&-restructures-scss

---
+++ index.js
@@ -0,0 +1,5 @@
+import {pokemonContainer, allPoke, searchPokemons, createPokemon } from "./javascript/functions.js";
+
+searchPokemons();
+createPokemon();
+allPoke();
---
+++ javascript/poke3enraya.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let turn = true;
+const moveArray = new Array(9).fill(null);
+function winner(i, j, k) {
+    if (moveArray[i] == moveArray[j] &&
+        moveArray[k] == moveArray[j] &&
+        moveArray[i] != null)
+        return true;
+        return false;
+    }
+const btnList = document.querySelectorAll('.game');
+btnList.forEach((btn, index) => {
+    btn.addEventListener('click', event => {
+        event.target.textContent = turn ? 'X' : "O";
+        moveArray[index] = turn;
+        if (winner(0, 1, 2) ||
+            winner(3, 4, 5) ||
+            winner(6, 7, 8) ||
+            winner(0, 3, 6) ||
+            winner(1, 4, 7) ||
+            winner(2, 5, 8) ||
+            winner(0, 4, 8) ||
+            winner(2, 4, 6))
+            document.querySelector('#winner').textContent = `Ganó el PokEquipo "${turn ? 'X' : 'O'}" `;
+        turn = !turn;
+    });
+});
+
